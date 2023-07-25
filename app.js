e7942e160968db7c6bf08d0d28f5f2d3f6edeaad
Hugo <hzerdan@gmail.com>
2023-07-25 12:33:28 -0300

    Clase89

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
+const express = require('express')
+const app = express()
+const puerto = 8085
 
-const http = require('http');
-
-const servidor = http.createServer( (req,res) => {
-
-    // res.writeHead(404);
-    // res.write('404 | Page not found');
-    res.write('Esta es mi página');
-    // res.end();
-    // console.log(req)
-
-    // res.writeHead(200, { 'Content-type': 'text/plain'});
-    // res.writeHead(200, { 'Content-type': 'application/json'});
-    // const persona = {
-    //     edad: 66,
-    //     nombre:'Hugo'
-    // }
-    // res.write( JSON.stringify(persona));
-
-    // Para devolver un csv
-    // res.setHeader('Content-Disposition','attachment;filename=lista.csv');
-    // res.writeHead(200, { 'Content-Type': 'application/csv'});
-    // res.write('id;nombre\n');
-    // res.write('1;Hugo\n');
-    // res.write('2;Horacio\n');
-    // res.write('3;Adriana\n');
-    // res.write('4;Ines\n');
-    // res.write('5;Lucía\n');
+// Servir contenido estático
+app.use( express.static('public') );
 
 
-    res.end()
+app.get('/hola-mundo',  (req, res) => {
+    res.send('Hola mundo tiene su propia pagina');
+});
+  
 
+app.get('*',  (req, res) => {
+    res.sendFile(__dirname+'/public/404.html');
 });
 
-servidor.listen(8085);
+
+app.listen(puerto,() => {
+    console.log (`Escuchando en el puerto ${puerto}`);
+});
