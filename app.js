6d235b9f9cf867783f16932542ac42ff8b6daf0d
Hugo <hzerdan@gmail.com>
2023-07-25 13:40:49 -0300

    Clase 90 completada

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const puerto = 8085
 app.use( express.static('public') );
 
 
-app.get('/hola-mundo',  (req, res) => {
-    res.send('Hola mundo tiene su propia pagina');
+app.get('/generic',  (req, res) => {
+    res.sendFile(__dirname+'/public/generic.html');
 });
   
+app.get('/elements',  (req, res) => {
+    res.sendFile(__dirname+'/public/elements.html');
+});
+ 
 
 app.get('*',  (req, res) => {
     res.sendFile(__dirname+'/public/404.html');
